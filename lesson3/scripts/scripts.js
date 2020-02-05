578ca0d1c002598186bd9ecd35d831de7f2c615a
aaronhathaway1 <aaronhathaway1@hotmail.com>
2020-02-04 22:20:32 -0700

    changes

---
+++ lesson3/scripts/scripts.js
@@ -0,0 +1,9 @@
+var date = new Date();
+
+var year = date.getFullYear();
+
+console.debug(year);
+
+document.getElementById('currentYear').innerHTML = year;
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
