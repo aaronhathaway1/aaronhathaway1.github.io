8ccc02ab46bac85ba3fe2a697ee911fd27227269
aaronhathaway1 <aaronhathaway1@hotmail.com>
2020-03-25 13:49:19 -0600

    changed images for preston 5

--- lesson5/scripts/main.js
+++ lesson5/scripts/main.js
@@ -1,11 +1,7 @@
 var date = new Date();
-
 var year = date.getFullYear();
 
-console.debug(year);
-
 document.getElementById('currentYear').innerHTML = year;
-
 document.getElementById('lastModified').innerHTML = document.lastModified;
 
 
