6029df4467909a79a484a58e657ac5e930ff34b8
aaronhathaway1 <aaronhathaway1@hotmail.com>
2020-03-25 20:57:16 -0600

    lesson 6 responsive img

---
+++ lesson61/lesson5/scripts/dateModified.js
@@ -0,0 +1,9 @@
+/* <!-- Copyright and Last Modified functions --> */
+
+var date = new Date();
+var year = date.getFullYear();
+
+console.debug(year);
+
+document.getElementById('currentYear').innerHTML = year;
+document.getElementById("lastModified").innerHTML = document.lastModified;
